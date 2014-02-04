e479e60bd49d66e3554774d496643e2c7f32645c
Suttipong Srikok <fisca2012@gmail.com>
2014-02-04 17:36:31 +0700

    Install "VirtueMart" extension and build "mod_helloworld" module.

---
+++ logs/com_virtuemart.log.php
@@ -0,0 +1,7 @@
+#
+#<?php die("Forbidden."); ?>
+
+2014-02-04 05:43:27 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at C:\AppServ\www\Joomla\libraries	cpdf
+2014-02-04 05:44:37 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at C:\AppServ\www\Joomla\libraries	cpdf
+2014-02-04 05:50:57 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at C:\AppServ\www\Joomla\libraries	cpdf
+2014-02-04 05:53:27 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at C:\AppServ\www\Joomla\libraries	cpdf
